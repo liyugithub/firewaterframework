1671e2da8b35f18e5fd59a4d53d02cf0b40fa8de
tspurway <tspurway@ac4af509-0941-0410-a2f2-1fa738afeeb2>
2008-08-11 12:09:26 +0000

    added groovy language and gmaven to pom

---
+++ trunk/src/main/java/org/firewaterframework/mappers/scripting/GroovyMapper.java
@@ -0,0 +1,20 @@
+package org.firewaterframework.mappers.scripting;
+
+import org.firewaterframework.mappers.Mapper;
+import org.firewaterframework.rest.Response;
+import org.firewaterframework.rest.Request;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: tspurway
+ * Date: Aug 1, 2008
+ * Time: 12:18:18 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class GroovyMapper extends Mapper
+{
+    public Response handle(Request request)
+    {
+        return null;
+    }
+}
