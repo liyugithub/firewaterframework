ecf042cc889d39e0a8fdb89410b925ae56b3908c
tspurway@gmail.com <tspurway@gmail.com@ac4af509-0941-0410-a2f2-1fa738afeeb2>
2010-08-11 20:13:52 +0000

    [maven-release-plugin]  copy for tag firewater-0.9.3.21

---
+++ tags/firewater-0.9.3.21/src/main/java/org/firewaterframework/mappers/jdbc/QueryHolder.java
@@ -0,0 +1,34 @@
+package org.firewaterframework.mappers.jdbc;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: tspurway
+ * Date: Feb 19, 2008
+ * Time: 7:32:23 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class QueryHolder
+{
+    String query;
+    String keyName;
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+
+    public String getKeyName() {
+        return keyName;
+    }
+
+    public void setKeyName(String keyName) {
+        this.keyName = keyName;
+    }
+
+    public boolean hasKey() {
+        return keyName != null && keyName.length() > 0;
+    }
+}
